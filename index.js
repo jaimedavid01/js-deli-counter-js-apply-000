81b751cdba92e76bdf9a7d1c6579dea2cab8a592
stellar-server-0187 <jaimedavid01@yahoo.com>
2020-10-06 18:27:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-function takeANumber(katzDeliLine, name){
-  katzDeliLine.push(name);
-  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+function takeANumber(){
+  var number = 0
+  number++
+  return number;
 }
+  
+ 
 function nowServing(katzDeliLine){
   if (katzDeliLine.length > 0){
     return `Currently serving ${katzDeliLine.shift()}.`;
